005fedfa4220dbbd89c65c9a3553cf1cfd89e9c7
Vishal Sharma <vsh1819@gmail.com>
2018-07-09 07:46:16 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,1 +1,1 @@
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 1, "M", "Black", "tame", 1);
